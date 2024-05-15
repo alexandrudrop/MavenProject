4db632b2df529384717e396d33e2c3a16c96ec73
alexandru.drop <alexandru.drop@gmail.com>
2024-05-15 22:14:19 +0300

    curs20 tema

--- src/main/java/curs20/tema/FindTheBook.java
+++ src/main/java/curs20/tema/FindTheBook.java
@@ -1,5 +1,6 @@
 package curs20.tema;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 import java.util.List;
@@ -17,9 +18,8 @@ public class FindTheBook extends BaseTest{
 	@Test
 	public void testFindTheBook() {
 		
-		List<WebElement> menuElements = driver.findElements(By.cssSelector("li[class='sc_tabs_title']"));
+		List<WebElement> menuElements = driver.findElements(By.cssSelector("li[class*='sc_tabs_title']"));
 		
-     //   String[] categories = {"New Releases", "Coming Soon", "Best Seller", "Award Winner"};
 		
         for (int i = 0; i < menuElements.size(); i++) {
             WebElement menuElement = menuElements.get(i);
@@ -41,6 +41,9 @@ public class FindTheBook extends BaseTest{
         menuElements.get(3).click();
         WebElement forest3 = driver.findElement(By.cssSelector("img[src=\"https://keybooks.ro/wp-content/uploads/2016/04/book2.jpg\"]"));
 		assertTrue(forest3.isDisplayed());
+		driver.findElement(By.cssSelector("a[href='the-forest']")).click();
+		driver.getCurrentUrl();
+		assertEquals(driver.getCurrentUrl(), "https://keybooks.ro/shop/the-forest/"); 
 
 	}
 }
--- src/main/java/curs21/IsEnabledExample.java
+++ src/main/java/curs21/IsEnabledExample.java
@@ -7,23 +7,33 @@ import org.testng.annotations.Test;
 
 import selenium.utils.BaseTest;
 
-public class IsEnabledExample extends BaseTest {
-	
+public class IsEnabledExample  extends BaseTest{
+
 	@Test
-	public void isEnabledTest(){
-		
-		JavascriptExecutor js = (JavascriptExecutor)driver;
+	public void isEnabledTest() {
+		JavascriptExecutor js =  (JavascriptExecutor)driver;
 		
 		driver.findElement(By.cssSelector("li[class='menu_user_login']")).click();
+
 		
 		WebElement passField = driver.findElement(By.id("password"));
+		
 		System.out.println(passField.isEnabled());
 		
-		js.executeScript("arguments[0]setAttribute('disabled', '')", passField);
+		js.executeScript("arguments[0].setAttribute('disabled', '')", passField);
+		
 		System.out.println("Displayed " + passField.isDisplayed());
+		
 		//passField.sendKeys("parola");
+	
+		
 		System.out.println(passField.isEnabled());
+
+		
 		
 	}
-
+	
+	
+	
+	
 }
